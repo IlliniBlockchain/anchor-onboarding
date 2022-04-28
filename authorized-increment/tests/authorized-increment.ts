78be76e9e390e3893edf89e035e4b0d41788362b
Alec Chen <alecchendev@gmail.com>
2022-04-27 21:49:40 -0500

    Add basic test for initialize

--- authorized-increment/tests/authorized-increment.ts
+++ authorized-increment/tests/authorized-increment.ts
@@ -1,16 +1,52 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { Program, web3, Provider } from "@project-serum/anchor";
 import { AuthorizedIncrement } from "../target/types/authorized_increment";
+import { expect } from "chai";
+
+async function createWallet(
+  provider: Provider,
+  lamports: number
+): Promise<web3.Keypair> {
+  const wallet = web3.Keypair.generate();
+  const fundTx = new web3.Transaction().add(
+    web3.SystemProgram.transfer({
+      fromPubkey: provider.wallet.publicKey,
+      toPubkey: wallet.publicKey,
+      lamports,
+    })
+  );
+
+  const response = await provider.send(fundTx);
+  return wallet;
+}
 
 describe("authorized-increment", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
-  const program = anchor.workspace.AuthorizedIncrement as Program<AuthorizedIncrement>;
+  const program = anchor.workspace
+    .AuthorizedIncrement as Program<AuthorizedIncrement>;
+
+  let authority: web3.Keypair;
+  let counter: web3.Keypair;
+
+  before(async () => {
+    authority = await createWallet(program.provider, 2 * 10 ** 6);
+    counter = new web3.Keypair();
+  });
 
   it("Is initialized!", async () => {
+    const start = 3;
+
     // Add your test here.
-    const tx = await program.rpc.initialize({});
+    const tx = await program.rpc.initialize(new anchor.BN(start), {
+      accounts: {
+        counter: counter.publicKey,
+        authority: authority.publicKey,
+        systemProgram: web3.SystemProgram.programId,
+      },
+      signers: [counter, authority],
+    });
     console.log("Your transaction signature", tx);
   });
 });
