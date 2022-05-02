bd3060ac135319eb3261f79ebce58e2859092d30
Alec Chen <alecchendev@gmail.com>
2022-05-01 21:57:05 -0500

    Fix program name for idl generation

--- authorized-increment/programs/authorized-increment/src/lib.rs
+++ authorized-increment/programs/authorized-increment/src/lib.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-mod counter {
+mod authorized_increment {
     use super::*;
 
     /// All that needs to go here is setting the initial
